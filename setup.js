6b8f37ab654c360689eac633f1fb059c3baa0d5f
lohvyn-m-beep <lohvyn-m@zorka-milk.com>
2026-09-14 18:49:33 +0300

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  const token = process.env.TELEGRAM_BOT_TOKEN;
 
+  const response = await fetch(
+    `https://api.telegram.org/bot${token}/setWebhook`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        url: "https://balance-bot-three.vercel.app/api/telegram"
+      })
+    }
+  );
+
+  const result = await response.json();
+
+  return res.status(200).json(result);
+}
